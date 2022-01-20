4236e2e9f4a96e01720a96e96ec97e6c34ee68cc
ESMERALDOJr <esmeraldo.log@hotmail.com>
2022-01-20 11:49:44 -0300

    Desafio 4 DIO C#

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Sequencia IJ 2
+
+Você deve fazer um programa que apresente a sequencia conforme o exemplo abaixo.
+
+Entrada
+Não há nenhuma entrada neste problema.
+
+Saída
+Imprima a sequencia conforme exemplo abaixo
+*/
+
+using System;
+
+namespace Desafio4
+{
+    class Desafio4
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 9; i += 2)
+            {
+                for(int j = 7; j >=5; j--)
+                {
+                    Console.WriteLine("I=" + i + " J=" + j);
+                }
+            }
+        }
+    }
+}
